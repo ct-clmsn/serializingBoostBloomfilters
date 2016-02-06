1dc024e6f953ede1527ab54a6d0827d5cbd057e4
ct <ct@>
2016-02-06 11:39:27 -0800

    reformatted demo

--- demo.cpp
+++ demo.cpp
@@ -13,8 +13,12 @@ int main(int argc, char** argv) {
    serializeable_counting_bloom_filter<string, NUM_BITS> bf;
    const string sentence("See Spot Run");
    stringstream ss(sentence);
+
    string buf;
-   while( ss >> buf ) { cout << buf << '\t' << bf.probably_contains(buf) << '\t' << bf.str() << endl; bf.insert(buf); }
+   while( ss >> buf ) { 
+      cout << buf << '\t' << bf.probably_contains(buf) << endl << bf.str() << endl; 
+      bf.insert(buf); 
+   }
    
 }
 
